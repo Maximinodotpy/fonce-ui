9be03d30162d010556b2360e85094d7e1f8e6a9b
Maximinodotpy <89255962+MaximMaeder@users.noreply.github.com>
2022-07-23 08:27:03 +0200

    changes

--- index.php
+++ index.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device th, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fonce UI</title>
 
     <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body class="background-main">
-    <div class="container-medium">
-        <div class="p-1 background-tertiary sticky-top">
+    <div class="container-large">
+        <div class="p-1 background-tertiary sticky-top flex flex-align-center">
             <h2>Fonce UI</h2>
         </div>
         <div class="flex child-p-1">
             <div class="background-secondary width-1-6 appear-at-medium">
                 <h6 class="mb-1">Components</h6>
-                <ul>
+                <ul class="child-mb-0-2">
                     <?php
 
 
@@ -33,14 +33,14 @@
                     ?>
                 </ul>
                 <h6 class="mb-1 mt-2">Document</h6>
-                <ul>
+                <ul class="child-mb-0-2">
                     <li><a href="">Usage</a></li>
                     <li><a href="">Classes</a></li>
                     <li><a href="#fsc">Full SASS Code</a></li>
                 </ul>
                 <h6 class="mb-1 mt-2">Other</h6>
-                <ul>
-                    <li><a href="https://github.com/Maximinodotpy/fonce-ui">GitHub</a></li>
+                <ul class="child-mb-0-2">
+                    <li><a target="_blank" href="https://github.com/Maximinodotpy/fonce-ui">GitHub</a></li>
                 </ul>
             </div>
             <div class="background-highlight flex-expand">
@@ -80,6 +80,16 @@
                     echo '<h1>Fonce UI</h1>';
                 } ?>
             </div>
+        </div>
+        <div class="p-1 background-tertiary">
+            Made By Maxim Maeder
+
+            <div>
+                <code>
+                https://cdn.jsdelivr.net/gh/Maximinodotpy/fonce-ui@latest/style/style.css
+                </code>
+            </div>
+
         </div>
     </div>
 
